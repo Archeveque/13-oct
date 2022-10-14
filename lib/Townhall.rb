e8f999180de4dc3e0e65c8347d4798d26a64c626
Archeveque <114493055+Archeveque@users.noreply.github.com>
2022-10-14 09:31:49 +0200

    Update Townhall.rb

--- lib/Townhall.rb
+++ lib/Townhall.rb
@@ -1,30 +1,29 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-townhall_url = "saint-cezaire-sur-siagne"
 
 townhall_mail = []
 townhall_name =[]
 
-def get_townhall_urls(townhall_name,townhall_mail)
+def get_townhall_urls(townhall_name,townhall_mail)      #scrap the name of the towns
 
 page = Nokogiri::HTML(URI.open("http://www.annuaire-des-mairies.com/val-d-oise.html"))
 
 page.xpath('//a[contains(@class,"lientxt")]').each do |name|
   #you can use //a[contains[.....]/@href to directly have the link (but my way was funnier)
-  townhall_url=name.text.downcase!.gsub(" ", "-")
-  townhall_email(townhall_url,townhall_name,townhall_mail)
-  puts "#{townhall_url} done."
+  townhall_url=name.text.downcase!.gsub(" ", "-")             #edit format for the name (to put it in the url) (((Saint Roger ->>> saint-roger)))
+  townhall_email(townhall_url,townhall_name,townhall_mail)    #scrap the email adress of the townhall
+  puts "#{townhall_url} done."                                #say wich town was scrapped
 end
-townhall_and_mail = townhall_name.zip(townhall_mail)
+townhall_and_mail = townhall_name.zip(townhall_mail)    #creating a hash with the name of the twon and the email
 puts townhall_and_mail
 end
 
 
-def townhall_email(townhall_url,townhall_name,townhall_mail)
+def townhall_email(townhall_url,townhall_name,townhall_mail)#scrap the email adress of the townhall using the town naùe as the url
 townhall_name.push (townhall_url)
 page = Nokogiri::HTML(URI.open("http://www.annuaire-des-mairies.com/95/#{townhall_url}.html"))
-page.xpath('//main[contains(@class,"page-content")]/section[last()-2]//tbody//tr[last()-1]//td[last()]').each do |mail|
+page.xpath('//main[contains(@class,"page-content")]/section[last()-2]//tbody//tr[last()-1]//td[last()]').each do |mail| #scrap the email adress
   townhall_mail.push(mail.text)
 end
 return townhall_mail
@@ -32,8 +31,8 @@ return townhall_name
 end
 
 
-
-def old_townhall_email(townhall_url)
+#/////////////////////////
+def old_townhall_email(townhall_url) #previous version of the code
 
 townhall_mail = []
 crypto_values = []
@@ -44,5 +43,6 @@ page.xpath('//main[contains(@class,"page-content")]/section[last()-2]//tbody//tr
   townhall_mail.push(mail.text)
 end
 end
+
 get_townhall_urls(townhall_name,townhall_mail)
 puts " "
